d98beea1bf6d9f2f3ac84f2c695a009d0c435ec7
chuncaso90 <72043079+chuncaso90@users.noreply.github.com>
2022-01-15 22:31:14 -0800

    icon color change when clicked

--- comps/ItemFilterIcons/index.js
+++ comps/ItemFilterIcons/index.js
@@ -7,17 +7,19 @@ import { MdAirportShuttle, MdHardware } from "react-icons/md";
 const Cont = styled.div`
   display: flex;
   width: 35%;
-  background-color: aliceblue;
+  /* background-color: aliceblue; */
   height: 75px;
   justify-content: space-between;
   align-items: center;
   display:flex;
-  font-size: 1.5rem;
+  font-size: 1.2rem;
+
 
 `;
 
 const filterhead = styled.p`
 `;
+
 
 
 
@@ -32,7 +34,15 @@ export default function ItemFilterIcons() {
     // const setTravel = () => showTravel(true);
     // const setUtils = () => showUtils(true);
 
-    const [filter, setFilter] = useState('all')
+    // const [filter, setFilter] = useState('all')
+    
+    // filtering trial
+
+    const [isBlue, setIsBlue] = useState(false);
+    const [isGreen, setIsGreen] = useState(false);
+    const [isRed, setIsRed] = useState(false);
+
+    //color change
 
   return (
     <Cont>
@@ -41,9 +51,23 @@ export default function ItemFilterIcons() {
       <MdAirportShuttle onClick={()=>{setTravel}}/>
       <MdHardware onClick={()=>{setUtils}}/>       */}
 
-      <GiForkKnifeSpoon onClick={()=>{setFilter('food')}}/>
+      {/* <GiForkKnifeSpoon onClick={()=>{setFilter('food')}}/>
       <MdAirportShuttle onClick={()=>{setFilter('travel')}}/>
-      <MdHardware onClick={()=>{setFilter('utils')}}/>    
+      <MdHardware onClick={()=>{setFilter('utils')}}/>     */}
+      
+
+      <GiForkKnifeSpoon 
+      onClick={() => setIsBlue(!isBlue)}
+      color={isBlue ? 'blue' : 'black'}
+      />
+      <MdAirportShuttle 
+      onClick={() => setIsGreen(!isGreen)}
+      color={isGreen ? 'green' : 'black'}
+      />
+      <MdHardware onClick={() => setIsRed(!isRed)}
+      color={isRed? 'red' : 'black'}
+      />    
+
       
     </Cont>
   );
