b63fae3ae1d12c468e59618a9088f80f85876c44
chuncaso90 <72043079+chuncaso90@users.noreply.github.com>
2022-01-15 21:50:25 -0800

    created item filter icons

---
+++ comps/ItemFilterIcons/index.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Cont = styled.div`
+  display: flex;
+  width: 30%;
+  background-color: aliceblue;
+  height: 75px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+
+export default function ItemFilterIcons() {
+
+  return (
+    <Cont>
+
+    </Cont>
+  );
+}
