182d47398bee69a85f07eaddef4256b3648ef089
JessicaW0 <jwu326@my.bcit.ca>
2022-01-16 16:03:22 -0800

    sum total works

--- pages/index.js
+++ pages/index.js
@@ -114,48 +114,6 @@ export default function Home() {
   //Add Total expenses from day
 
 
-  /*const totalSum = {
-    expenses: [],
-
-    setExpenses(expenseName,expenseType, expensePrice) {
-        account.expenses.push({
-            expenseName: expenseName,
-            expensePrice: expensePrice,
-            expenseType: expenseType,
-        });
-    },
-
-    getTotalSumSummary() {
-
-        return totalSum.expenses.reduce((result, expense) => result + expense.expensePrice, 0)
-        
-    }
-}*/
-
-/*
-function DisplayExpense (){
-const {expenses, setExpenses} = useState [0]
-
-return{
-  Total Expense:{''}
-  <span className="text-success">
-    ${' '}
-    {expenses.reduce((accumulator, currentValue) => {
-      return (accumulator += parseInt(currentValue.amount))
-    }, 0)}
-
-    <form/>
-    <List expenses ={expenses}/>
-}
-}
-*/
-
-
- 
- 
- 
-
- 
 
   const sum = filteredExpenses.map(x => x.price)
 
@@ -163,17 +121,14 @@ function add(array){
   var total = 0;
 
   for ( var i = 0; i < sum.length; i++){
-    total += array[i]
-    console.log(array[i])
-     console.log(total)
+    total += +sum[i]
   }
   return total;
- 
 }
 
-console.log(add(sum))
+//console.log(add(sum))
 
- console.log(filteredExpenses.map(x => x.price)); 
+ //console.log(filteredExpenses.map(x => x.price)); 
   //Set Daily Budget
 
   //Calculate Budget and Total Expense Difference
